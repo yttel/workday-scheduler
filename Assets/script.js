ae454e2e147115d2baa49b3addf2b57c67612c4f
BytteL <lettybedard@gmail.com>
2020-01-28 20:52:50 -0500

    mocked up, loads in cal

---
+++ Assets/script.js
@@ -0,0 +1,125 @@
+// js for workdayt calendar
+// written by Letty Bedard
+
+$(function(){
+  
+  //  DECLARATIONS 
+  let today = moment().format("dddd MMMM Do YYYY");
+  let toDoList = [{ //blank list (nothing in local)
+    hour: "06",
+    task: ""
+  },{
+    hour: "07",
+    task: ""
+  },{
+    hour: "08",
+    task: ""
+  },{  
+    hour: "09",
+    task: ""
+  },{
+    hour: "10",
+    task: ""
+  },{
+    hour: "11",
+    task: ""
+  },{
+    hour: "12",
+    task: ""
+  },{
+    hour: "13",
+    task: ""
+  },{
+    hour: "14",
+    task: ""
+  },{
+    hour: "15",
+    task: ""
+  },{
+    hour: "16",
+    task: ""
+  },{
+    hour: "17",
+    task: ""
+  },{
+    hour: "18",
+    task: ""
+  },{
+    hour: "19",
+    task: ""
+  },{
+    hour: "20",
+    task: ""
+  },{
+    hour: "21",
+    task: ""
+  },{
+    hour: "22",
+    task: ""
+  }];
+  let storedTasks = JSON.parse(localStorage.getItem("storedList"));
+
+  //  REFERENCES
+  let calendarSpot = $("#middlePart");
+
+  //  FUNCTIONS
+
+  function hourTime(time){ //convert 24hr to 12hr
+    let timeInt = parseInt(time);
+    if (timeInt <= 12){
+      return (`${timeInt} a.m.`);
+    }
+    else {
+      return (`${timeInt % 12} p.m.`);
+    }
+  }
+
+  function addARow(time, words){
+    let row = $("<div>")
+                .addClass("row w-100 mx-0")
+                .attr("data-hour", time);
+    row.append($("<div>")
+                .text(hourTime(time))
+                .addClass("col-lg-2 text-center w-100"));
+    row.append($("<div>")
+                .text(words)
+                .addClass("col-lg-8 text-center w-100")
+                .attr("data-hour", time));
+    row.append($("<div>")
+                .addClass("col-lg-2 w-100 text-center")
+                .append($("<i>")
+                        .addClass("far fa-save fa-3x")
+                        .attr("data-hour", time)));    
+    calendarSpot.append(row);
+  }
+
+  function renderCalendar(){
+    $("#today").text(today);
+    calendarSpot.empty();
+    if (storedTasks){
+      storedTasks.forEach(function(element){
+        addARow(element.hour, element.task);
+      });
+    }
+    else {
+      localStorage.setItem("storedList", JSON.stringify(toDoList));
+
+      toDoList.forEach(function(element){
+        addARow(element.hour, element.task);
+      });
+    }
+  }
+
+  //takes the text and stores it in the location (string from data-hour attr)
+  function makeEntry(text, location){
+    let time = parseInt(location);
+    storedTasks[time-6].text(text);
+    localStorage.setItem("storedList", JSON.stringify(storedTasks)); 
+  }
+
+
+  //  EVENT LISTENERS
+
+  renderCalendar();
+
+});
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # workday-scheduler
 Assignment 5 - Work Day Scheduler
+
+## Assignment Description
+
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the Moment.js library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+## Links
+
+[github source](https://github.com/yttel/workday-scheduler)
+
+[deployed site](https://yttel.github.io/workday-scheduler/)
+
+## Screenshots
+
+![Initial screen](./Assets/Images/quizInst.PNG)
+
+![During use](./Assets/Images/quizQuest.PNG)
